484d8d03997deb072a6848f30c04f7b34b5c865c
SaxoDevs <112919860+saxodevs@users.noreply.github.com>
2026-05-01 01:10:24 +0000

    Fixing env error in local devs

--- src/main/java/com/saxodevs/pos/PosApplication.java
+++ src/main/java/com/saxodevs/pos/PosApplication.java
@@ -1,19 +1,13 @@
 package com.saxodevs.pos;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.UUID;
 
 @SpringBootApplication
 public class PosApplication {
 
-
 	public static void main(String[] args)
 	{
-
 		SpringApplication.run(PosApplication.class, args);
-
 	}
 
 }
